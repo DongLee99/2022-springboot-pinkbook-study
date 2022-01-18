d15a49c6de52cf0ea25eea086fe0d9042837c1fe
DongLee99 <55686088+DongLee99@users.noreply.github.com>
2022-01-18 13:30:26 +0900

    init

---
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Value("${property.test.name")
+	private String propertyTestName;
+
+	@Value("${propertyTest")
+	private String propertyTest;
+
+	@Value("${noKey:default value}")
+	private String defaultValue;
+
+	@Value("${propertyTestList}")
+	private String [] propertyTestArray;
+
+	@Value("#{'${propertyTestList}'.split(',')}")
+	private List<String> propertyTestList;
+
+	@Test
+	public void testValue() {
+		assertThat(propertyTestName).isEqualTo("property depth test");
+		assertThat(propertyTest).isEqualTo("test");
+
+		assertThat(defaultValue).isEqualTo("default value");
+
+		assertThat(propertyTestArray[0]).isEqualTo("a");
+
+	}
+
+}
